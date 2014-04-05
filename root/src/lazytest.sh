75e735d372d0e3f612b202236d91545e586525e9
Ragnar <ragnarls08@gmail.com>
2014-04-05 21:00:53 +0200

    added lazytest, runs all tests and outputs to stdout

---
+++ root/src/lazytest.sh
@@ -0,0 +1,102 @@
+echo "test core 001"
+./jlc tester/testsuite/good/core001.jl
+echo "test core 002"
+./jlc tester/testsuite/good/core002.jl
+echo "test core 003"
+./jlc tester/testsuite/good/core003.jl
+echo "test core 004"
+./jlc tester/testsuite/good/core004.jl
+echo "test core 005"
+./jlc tester/testsuite/good/core005.jl
+echo "test core 006"
+./jlc tester/testsuite/good/core006.jl
+echo "test core 007"
+./jlc tester/testsuite/good/core007.jl
+echo "test core 008"
+./jlc tester/testsuite/good/core008.jl 
+echo "test core 009"
+./jlc tester/testsuite/good/core009.jl 
+echo "test core 010"
+./jlc tester/testsuite/good/core010.jl 
+echo "test core 011"
+./jlc tester/testsuite/good/core011.jl 
+echo "test core 012"
+./jlc tester/testsuite/good/core012.jl 
+echo "test core 013"
+./jlc tester/testsuite/good/core013.jl 
+echo "test core 014"
+./jlc tester/testsuite/good/core014.jl 
+echo "test core 015"
+./jlc tester/testsuite/good/core015.jl 
+echo "test core 016"
+./jlc tester/testsuite/good/core016.jl 
+echo "test core 017"
+./jlc tester/testsuite/good/core017.jl 
+echo "test core 018"
+./jlc tester/testsuite/good/core018.jl 
+echo "test core 019"
+./jlc tester/testsuite/good/core019.jl 
+echo "test core 020"
+./jlc tester/testsuite/good/core020.jl 
+echo "test core 021"
+./jlc tester/testsuite/good/core021.jl 
+echo "test core 022"
+./jlc tester/testsuite/good/core022.jl 
+
+echo ""
+echo ""
+echo "testing bad files"
+echo ""
+echo ""
+echo "test bad 001"
+./jlc tester/testsuite/bad/bad001.jl
+echo "test bad 002"
+./jlc tester/testsuite/bad/bad002.jl
+echo "test bad 003"
+./jlc tester/testsuite/bad/bad003.jl
+echo "test bad 004"
+./jlc tester/testsuite/bad/bad004.jl
+echo "test bad 005"
+./jlc tester/testsuite/bad/bad005.jl
+echo "test bad 006"
+./jlc tester/testsuite/bad/bad006.jl
+echo "test bad 007"
+./jlc tester/testsuite/bad/bad007.jl
+echo "test bad 008"
+./jlc tester/testsuite/bad/bad008.jl
+echo "test bad 009"
+./jlc tester/testsuite/bad/bad009.jl
+echo "test bad 010"
+./jlc tester/testsuite/bad/bad010.jl
+echo "test bad 011"
+./jlc tester/testsuite/bad/bad011.jl
+echo "test bad 012"
+./jlc tester/testsuite/bad/bad012.jl
+echo "test bad 013"
+./jlc tester/testsuite/bad/bad013.jl
+echo "test bad 015"
+./jlc tester/testsuite/bad/bad015.jl
+echo "test bad 016"
+./jlc tester/testsuite/bad/bad016.jl
+echo "test bad 017"
+./jlc tester/testsuite/bad/bad017.jl
+echo "test bad 018"
+./jlc tester/testsuite/bad/bad018.jl
+echo "test bad 019"
+./jlc tester/testsuite/bad/bad019.jl
+echo "test bad 020"
+./jlc tester/testsuite/bad/bad020.jl
+echo "test bad 021"
+./jlc tester/testsuite/bad/bad021.jl
+echo "test bad 022"
+./jlc tester/testsuite/bad/bad022.jl
+echo "test bad 023"
+./jlc tester/testsuite/bad/bad023.jl
+echo "test bad 024"
+./jlc tester/testsuite/bad/bad024.jl
+echo "test bad 025"
+./jlc tester/testsuite/bad/bad025.jl
+echo "test bad 026"
+./jlc tester/testsuite/bad/bad026.jl
+echo "test bad 027"
+./jlc tester/testsuite/bad/bad027.jl
